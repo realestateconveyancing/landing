a3ac8cb5f17e55035b2d307bed569c5a9206e46b
Muhammad Uzair <muzair.dev@gmail.com>
2020-10-07 15:39:06 +0500

    add ui

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,51 @@
 import React from 'react'
-import { Link } from 'gatsby'
-// import BlankTemplate from '../components/common/layout/blank-temp'
-import Image from '../components/common/image'
-import Home from '../components/home'
+import { useStaticQuery, graphql } from 'gatsby'
+import BackgroundImage from 'gatsby-background-image'
+import { Button, Row, Col } from 'antd'
 import Layout from '../components/common/layout'
 import SEO from '../components/common/seo'
+import { homeStyles } from '../styles'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Home />
-    {/* <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image imgName="person-writing.jpg" />
-    </div>
-    <Link to="/styled">Go to page 2</Link> */}
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      desktop: file(relativePath: { eq: "bg-img.jpg" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+
+  const imageData = data.desktop.childImageSharp.fluid
+  return (
+    <>
+      <SEO title="Home" />
+      <BackgroundImage
+        Tag="section"
+        className="hero-bg"
+        fluid={imageData}
+        backgroundColor="#040e18"
+      >
+        <Layout>
+
+          <Row style={homeStyles.contentRow}>
+            <Col span={6} />
+            <Col span={6} style={homeStyles.butnsWrap}>
+              <Button type="primary" style={homeStyles.contentBtn}>Sell A House </Button>
+            </Col>
+            <Col span={6} style={homeStyles.butnsWrap}>
+              <Button type="primary" style={homeStyles.contentBtn}>Sell A House </Button>
+            </Col>
+            <Col span={6} />
+          </Row>
+
+        </Layout>
+      </BackgroundImage>
+    </>
+  )
+}
 
 export default IndexPage
---
+++ src/styles/index.js
@@ -0,0 +1,28 @@
+export const homeStyles = {
+  contentRow: {
+    marginTop: '35vh',
+  },
+  contentBtn: {
+    height: '40px',
+    width: '300px',
+  },
+  footer: {
+    marginBottom: '20px',
+  },
+  footerText: {
+    color: 'white',
+    fontWeight: 'bold',
+
+  },
+  footerTextLink: {
+    color: 'white',
+    fontWeight: 'bold',
+    textDecoration: 'none',
+    cursor: 'pointer',
+    marginRight: '10px',
+
+  },
+}
+export default {
+  homeStyles,
+}
